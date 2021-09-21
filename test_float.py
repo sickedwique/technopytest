68bbc0be9549542d41e5bd015c0211ce44d21433
unknown <coolerjane@gmail.com>
2021-09-21 22:13:38 +0300

    parametrization

--- test_float.py
+++ test_float.py
@@ -1,3 +1,14 @@
+import pytest
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("1.0 and 1.0", 1.0),
+    ("1.0 or 1.0", 1.0),
+])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
 def test_float():
     assert float(1) == 1.0
     assert float(-1) == -1.0
@@ -14,5 +25,3 @@ def test_float():
     assert 1.0 - 1 == 0.0
     assert 1.0 * 1 == 1.0
     assert 1.0 / 1 == 1.0
-    assert 1.0 and 1.0 == 1.0
-    assert 1.0 or 1.0 == 1.0
--- test_set.py
+++ test_set.py
@@ -1,3 +1,15 @@
+import pytest
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("{1, 2, 3}.intersection({3, 4, 5})", {3}),
+    ("{1, 2, 3}.difference({3, 4, 5})", {1, 2}),
+    ("{1, 2, 3}.union({3, 4, 5})", {1, 2, 3, 4, 5}),
+])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+
 def test_set():
     assert set([-3, -3, 3, 3, -2, 1, 2, -1, 0, 0]) == {-3, -2, -1, 0, 1, 2, 3}
     assert set('1230003212312312002000120') == {'0', '1', '2', '3'}
