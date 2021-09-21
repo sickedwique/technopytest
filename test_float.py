2e880c4a2828486b31c6e6a5e362bde0a3497747
unknown <coolerjane@gmail.com>
2021-09-21 21:39:25 +0300

    add test for float

---
+++ test_float.py
@@ -0,0 +1,18 @@
+def test_float():
+    assert float(1) == 1.0
+    assert float(-1) == -1.0
+    assert 1.0 + 2.1 == 3.1
+    assert 1.0 + (-2.1) == -1.1
+    assert 1.0 - 0.5 == 0.5
+    assert 1.0 - 1.5 == -0.5
+    assert 1.0 - (-1.5) == 2.5
+    assert 1.0 * 2.0 == 2.0
+    assert 1.0 * (-2.0) == -2.0
+    assert 1.0 / 2.0 == 0.5
+    assert 1.0 / (-2.0) == -0.5
+    assert 1.0 + 1 == 2.0
+    assert 1.0 - 1 == 0.0
+    assert 1.0 * 1 == 1.0
+    assert 1.0 / 1 == 1.0
+    assert 1.0 and 1.0 == 1.0
+    assert 1.0 or 1.0 == 1.0
